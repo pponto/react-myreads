5945da8809a0d361ccce66d7df7c45f28672d127
pponto <pedromoreira@gmail.com>
2018-08-14 19:41:46 -0300

    git init

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import { Route } from 'react-router-dom'
+import SearchBooks from './SearchBooks'
+import ListBooks from './ListBooks'
+
+class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    })
+  }
+
+  updateBook = (book, shelf) => {
+    book.shelf = shelf;
+    const results = this.state.books.filter(currentbook => currentbook.id !== book.id)
+    results.push(book)
+
+    this.setState({ books: results })
+
+    BooksAPI.update(book.shelf)
+  }
+
+  render() {
+    return (
+      <div className="app">
+
+        <Route exact path='/' render={() => (
+          <ListBooks
+            onUpdateBook={this.updateBook}
+            books={this.state.books}
+          />
+        )}/>
+
+        <Route exact path='/search' render={() => (
+          <SearchBooks
+            onUpdateBook={this.updateBook}
+            books={this.state.books}
+          />
+        )}/>
+
+      </div>
+    )
+  }
+}
+
+export default BooksApp
